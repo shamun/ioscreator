6fcd4f6db666c873dcce025771912a4ae21f7802
Shamun <shamun.toha@gmail.com>
2017-03-08 11:49:32 +0100

    added new one

---
+++ 0-Objective-C_STRING/test-logic/main.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    NSString *my_url_as_string = @"tptiphone://test";
+    NSLog(my_url_as_string);
+  }
+  
+  return 0;
+  
+}
