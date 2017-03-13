ae0a70d8343a9d32decda38f1684b8b0fde2f696
Shamun <shamun.toha@gmail.com>
2017-03-13 07:59:08 +0100

    Push nightly

--- 1-Objective-C_Button_Image/OKK/ViewController.m
+++ 1-Objective-C_Button_Image/OKK/ViewController.m
@@ -1,43 +1,43 @@
-//
-//  ViewController.m
-//  OKK
-//
-//  Created by Sun on 09/03/17.
-//  Copyright © 2017 Sun. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
-  
-  UIButton *but= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  //UIButton *but= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  UIButton *but = [[UIButton alloc] init];
+  [but setTitle:@"ABCD" forState:UIControlStateNormal];
+  [but setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
   [but addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-  [but setFrame:CGRectMake(52, 252, 215, 40)];
-  [but setTitle:@"Login" forState:UIControlStateNormal];
-  [but setExclusiveTouch:YES];
   
-  // if you like to add backgroundImage else no need
+  int startHeight = 167;
+  int frameHeight = self.view.frame.size.height - startHeight;
+  [but setFrame:CGRectMake(0, startHeight, 320, frameHeight)];
+  
+  [but setTranslatesAutoresizingMaskIntoConstraints:false];
   [but setImage:[UIImage imageNamed:@"Image"] forState:UIControlStateNormal];
   [self.view addSubview:but];
+  
+  
+  NSDictionary *viewsDict = @{@"button" : but};
+  [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[button]-0-|"
+                                                                    options:0 metrics:nil views:viewsDict]];
+  NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:but attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
+  constraint.active = true;
+  [self.view addConstraint:constraint];
+  [self.view layoutIfNeeded];
 }
 
 
--(void) buttonClicked:(UIButton*)sender
-{
+-(void) buttonClicked:(UIButton*)sender {
   NSLog(@"you clicked on button %@", sender.tag);
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
 }
 
 
